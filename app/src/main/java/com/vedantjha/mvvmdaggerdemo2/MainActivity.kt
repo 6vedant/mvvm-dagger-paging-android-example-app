18fa29e72cef3e4bf307ae7e27b40fd1455f331b
Vedant Jha <6vedant@gmail.com>
2023-05-02 23:35:53 +0530

    added recyclerview and enabled databinding & viewbinding

--- app/src/main/java/com/vedantjha/mvvmdaggerdemo2/MainActivity.kt
+++ app/src/main/java/com/vedantjha/mvvmdaggerdemo2/MainActivity.kt
@@ -2,10 +2,15 @@ package com.vedantjha.mvvmdaggerdemo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vedantjha.mvvmdaggerdemo2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
