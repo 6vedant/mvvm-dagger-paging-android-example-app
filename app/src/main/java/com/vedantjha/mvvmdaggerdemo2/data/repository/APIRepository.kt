9574708c5ca00ac33de09509889f11b2da6469f3
Vedant Jha <6vedant@gmail.com>
2023-05-02 23:13:45 +0530

    Added pagination and glide lib in build.gradle(app)

---
+++ app/src/main/java/com/vedantjha/mvvmdaggerdemo2/data/repository/APIRepository.kt
@@ -0,0 +1,9 @@
+package com.vedantjha.mvvmdaggerdemo2.data.repository
+
+import com.vedantjha.mvvmdaggerdemo2.api.APIService
+import javax.inject.Inject
+
+class APIRepository @Inject constructor(private val apiService: APIService) {
+
+
+}
